530f48e2821174045693a644124d8e08fbd34550
mferrara <mf8yk@virginia.edu>
2015-02-13 09:50:02 -0600

    add drupal behavior to show-hide

--- js/shanti-main-kmaps.js
+++ js/shanti-main-kmaps.js
@@ -1,5 +1,8 @@
 
-			  // show-hide resource side-column
-			  $("button.view-resources").click( function() {
-			          $(this).toggleClass( "show",'fast' );
-			   });
+Drupal.behaviors.kmapsOffCanvasToggle = {
+  attach: function (context, settings) {
+		$("button.view-resources").click( function() { 		// show-hide resource side-column
+		        $(this).toggleClass( "show",'fast' );
+		});
+  }
+}
