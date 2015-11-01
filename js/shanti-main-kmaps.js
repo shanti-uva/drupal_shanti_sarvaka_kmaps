2347c1985764575f765f3d19df3120196fb32350
dukarla <artesano@vpn-user-1-61.itc.Virginia.EDU>
2015-11-01 06:16:32 -0600

    testing breadcrumb fixes

--- js/shanti-main-kmaps.js
+++ js/shanti-main-kmaps.js
@@ -1,35 +1,4 @@
-(function ($) {
-
-	Drupal.behaviors.shantiKmapsHideBreadcrumbsSubjects = {
-		attach: function (context, settings) {
-			if(context == window.document) {
-
-			$(window).load(function() {
-				$(".front .page-title-text:contains('Subjects')").remove(".breadwrap");
-			});	
-
-			}
-		} // end context = document
-	};
-
-
-	Drupal.behaviors.shantiKmapsHideBreadcrumbsEarth = {
-		attach: function (context, settings) {
-			if(context == window.document) {
-
-			$(window).load(function() {
-				$(".breadcrumb a:contains('Earth')").parents("li").remove();
-			});
-
-			}
-		} // end context = document
-	};
-
-}) (jQuery);
-
-
-
-
+// (function ($) {
 
 	/***
 
@@ -54,4 +23,4 @@
 	**/
 
 
-
+// }) (jQuery);
