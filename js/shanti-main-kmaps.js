8b1a277c4d36ab8819a65d3d931cfbddfa445480
dukarla <artesano@Marcos-Mac.local>
2016-03-02 19:33:42 -0600

    ending test

--- js/shanti-main-kmaps.js
+++ js/shanti-main-kmaps.js
@@ -10,38 +10,6 @@
 	  }
 	};
 
-	Drupal.behaviors.kmapsVideoTabID = {
-	  attach: function (context, settings) {
-	  	if(context == window.document) {
-
-				if($(".nav-pills li.active a:contains('Audio-Video')").length > 0 ) {
-					$("body").addClass("tab-video");
-				
-				} else {
-					
-					if($(".nav-pills li.active a:contains('Audio-Video')").length < 0 ) {
-
-						$("body").removeClass("tab-video");
-					}
-
-				}
-		  	
-		}
-	  }
-	};
-
 }) (jQuery);
 
 
-
-
-/***
-
-Drupal.behaviors.kmapsOffCanvasToggle = {
-  attach: function (context, settings) {
-		$("button.view-resources").click( function() { 		// show-hide resource side-column
-		        $(this).toggleClass( "show",'fast' );
-		});
-  }
-};
-**/
