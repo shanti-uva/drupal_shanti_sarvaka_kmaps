4e14392be4616aa57cb088978174affddc2fc806
dukarla <artesano@Marcos-Mac.local>
2016-04-17 08:23:23 -0500

    moving equal-height script to shanti-main.js

--- js/shanti-main-kmaps.js
+++ js/shanti-main-kmaps.js
@@ -10,21 +10,6 @@
 	  }
 	};
 
-
-	Drupal.behaviors.kmapsEqualHeights = {
-  		attach: function (context, settings) {
-			if(context == window.document) {        
-
-			    $(document).on('ajaxSuccess', function(){
-			      $('.equal-height').first().matchHeight({
-			          target: $('.equal-height.col-xs-12');
-			      });
-			    });   
-
-    		}
-  		} 
-	};
-
 }) (jQuery);
 
 
