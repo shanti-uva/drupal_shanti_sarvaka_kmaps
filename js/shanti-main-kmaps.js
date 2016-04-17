50481ca689861314622a3f7955c8b1567ca3d4af
dukarla <artesano@Marcos-Mac.local>
2016-04-17 12:20:56 -0500

    testing Kmaps custom equal-heights script

--- js/shanti-main-kmaps.js
+++ js/shanti-main-kmaps.js
@@ -1,6 +1,6 @@
 (function ($) {
 
-	Drupal.behaviors.kmapsSubjectsHomepage = {
+	Drupal.behaviors.shantiKmapsSubjectsHomepage = {
 	  attach: function (context, settings) {
 	  	if(context == window.document) {
 			if($(".feature-carousel-tabpanel").length ) {
@@ -9,6 +9,32 @@
 		}
 	  }
 	};
+
+	Drupal.behaviors.shantiKmapsEqualHeights = {
+	  attach: function (context, settings) { 
+	    if(context == window.document) {        
+	        // $(document).ajaxSuccess(function() {
+	        $(document).on('ajaxSuccess', function(){
+	          $('.main-col .equal-height').matchHeight({
+	              target: $('.equal-height.col-xs-12')
+	          });
+	        });    
+	       
+	            // $(document).ajaxSuccess(function() {
+	            $('.content-resources a,.panel-heading a').bind( 'click', function() {
+
+	              $(document).on('ajaxSuccess', function(){
+	                $('.main-col .equal-height').matchHeight({
+	                  target: $('.equal-height.col-xs-12')
+	                }); 
+	              });
+
+	            });  
+
+	         // $(window).bind('load orientationchange resize', Drupal.behaviors.shantiEqualHeights );  
+	    }
+	  } 
+	};	
 
 }) (jQuery);
 
