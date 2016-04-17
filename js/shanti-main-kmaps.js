cc93edc6ba5aa661b4c1f51e6543ae14ce7360d9
dukarla <artesano@Marcos-Mac.local>
2016-04-17 15:49:00 -0500

    testing Kmaps custom equal-heights script

--- js/shanti-main-kmaps.js
+++ js/shanti-main-kmaps.js
@@ -30,9 +30,11 @@
 	            });  
 
 	            $('.panel-group a').bind( 'click', function() {
+	              $(document).on('ajaxSuccess', function(){ 	
 	                $('.main-col .equal-height').matchHeight({
 	                  target: $('.equal-height.col-xs-12')
 	                }); 
+	              });  
 	            }); 
 
 	         // $(window).bind('load orientationchange resize', Drupal.behaviors.shantiEqualHeights );  
