ba96107e3e701244fc3d4fa8feaf147758475c68
dukarla <Artesano@vpn-user-1-67.itc.virginia.edu>
2016-08-09 18:39:35 -0500

    testing search flyout height/scrolling script

--- js/shanti-main-kmaps.js
+++ js/shanti-main-kmaps.js
@@ -11,18 +11,22 @@
 	};
 
 	 Drupal.behaviors.searchPanelHeightKMaps = {
-	   attach: function (context, settings) {
-	      if (context == document) {
-	   
-	            var height = $(window).height();
-	            var viewheightKMaps = (height) - 240;
+   	   attach: function (context, settings) {
+        if (context == document) {
 
-	            viewheightKMaps = parseInt(viewheightKMaps) + 'px';
-	            $(".view-wrap").css('height', viewheightKMaps);
+          // *** SEARCH *** adapt search panel height to viewport for scrolling treenav area
+          Drupal.ShantiSarvaka.searchTabHeightKMaps = function() {    
+            var height = $(window).height();
+            var viewheightKMaps = (height) - 240;
 
-	      }
-	  }
-	};
+            viewheightKMaps = parseInt(viewheightKMaps) + 'px';
+            $(".view-wrap").css('height', viewheightKMaps);
+
+          };
+
+        }
+      }
+  };
 
 
 
