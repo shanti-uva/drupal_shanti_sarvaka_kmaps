ed68f399416c44666e72e1ba2a1fd2cb06317393
dukarla <Artesano@vpn-user-1-67.itc.virginia.edu>
2016-08-09 18:04:16 -0500

    testing search flyout height/scrolling script

--- js/shanti-main-kmaps.js
+++ js/shanti-main-kmaps.js
@@ -9,6 +9,26 @@
 		}
 	  }
 	};
+
+	 Drupal.behaviors.searchPanelHeightKMaps = {
+	   attach: function (context, settings) {
+	      if (context == document) {
+	   
+	            var height = $(window).height();
+	            var viewheightKMaps = (height) - 240;
+
+	            viewheightKMaps = parseInt(viewheightKMaps) + 'px';
+	            $(".view-wrap").css('height', viewheightKMaps);
+
+	      }
+	  }
+	};
+
+
+
+
+
+
 /*
 	Drupal.behaviors.shantiKmapsEqualHeights = {
 	  attach: function (context, settings) { 
--- js/shanti-search-kmaps.js
+++ js/shanti-search-kmaps.js
@@ -1,17 +1,5 @@
 (function ($) { 
 
- Drupal.behaviors.searchPanelHeightKMaps = {
-   attach: function (context, settings) {
-      if (context == document) {
-   
-            var height = $(window).height();
-            var viewheightKMaps = (height) - 240;
 
-            viewheightKMaps = parseInt(viewheightKMaps) + 'px';
-            $(".view-wrap").css('height', viewheightKMaps);
-
-      }
-    }
-  };
 
 }(jQuery));
