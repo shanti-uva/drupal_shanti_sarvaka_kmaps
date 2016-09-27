1749a7699e9c53c315420d20915f19d75e36a9d6
Edward Garrett <edward@pinedrop.com>
2016-09-27 16:41:31 +0100

    Adjust height of lower panel of fly out in places search

--- js/shanti-main-kmaps.js
+++ js/shanti-main-kmaps.js
@@ -13,20 +13,23 @@
 	 Drupal.behaviors.searchPanelHeightKMaps = {
    	   attach: function (context, settings) {
         if (context == document) {
+          var $extruder = $('#search-flyout .extruder-content');
+          var $inputsec = $('#kmaps-search .input-section');
+          var input_height = 225; //height of initial input section including expanded search
 
           // *** SEARCH *** adapt search panel height to viewport for scrolling treenav area
-          Drupal.ShantiSarvaka.searchTabHeightKMaps = function() {    
-            var height = $(window).height();
-            var viewheightKMaps = (height) - 342;
-
+          Drupal.ShantiSarvaka.searchTabHeightKMaps = function() {
+            var extruder_height = $extruder.height();
+            if ($extruder.css('display', 'block')) {
+              input_height = $inputsec.height();
+            }
+            var viewheightKMaps = (extruder_height) - input_height - 75;
             viewheightKMaps = parseInt(viewheightKMaps) + 'px';
             $(".view-wrap").css('height', viewheightKMaps);
-
           };
 
-	      Drupal.ShantiSarvaka.searchTabHeightKMaps();
-	       $(window).bind('load orientationchange resize', Drupal.ShantiSarvaka.searchTabHeightKMaps );
-
+          Drupal.ShantiSarvaka.searchTabHeightKMaps();
+          $(window).bind('load orientationchange resize', Drupal.ShantiSarvaka.searchTabHeightKMaps);
         }
        }
   	};
@@ -37,36 +40,36 @@
 
 /*
 	Drupal.behaviors.shantiKmapsEqualHeights = {
-	  attach: function (context, settings) { 
-	    if(context == window.document) {  
+	  attach: function (context, settings) {
+	    if(context == window.document) {
 
 	        $(window).load(function() {
 		        	function set_equal_heights() {
 			          	$('.main-col .equal-height').matchHeight({
 			              target: $('main.main-wrapper')
 			          	});
-		            }; 
-		            window.setTimeout( set_equal_heights, 1000 );	             
+		            };
+		            window.setTimeout( set_equal_heights, 1000 );
 	        });
 
 
 	         // $(window).bind('load orientationchange resize', Drupal.behaviors.shantiEqualHeights );
 
 	    }
-	  } 
+	  }
 	};
 */
 
 
 
-	
+
 /*
 	Drupal.behaviors.shantiKmapsEqualHeightsAccordionAction = {
 	  attach: function (context, settings) {
 	  	if(context == window.document) {
 
             $('#accordion').on('shown.bs.collapse', function (e) {
-            	
+
             	var positionScrollTop = $(window).scrollTop();
 
     			$('.main-col .equal-height').matchHeight({
@@ -84,13 +87,13 @@
 
 
             $('#sidebar-first a.use-ajax').on( 'click', function() {
-		                
+
 				$(document).ajaxSuccess(function() {
 		        	// $(document).on('ajaxSuccess', function(){
 		          $('.main-col .equal-height').matchHeight({
 		              target: $('.equal-height.col-md-9')
 		          });
-		        });  
+		        });
             });
 
 
@@ -105,13 +108,13 @@
 	  if(context == window.document) {
 
 	    $( window ).load(function() {
-	      if($(".front.page-subjects").length ) { 
+	      if($(".front.page-subjects").length ) {
 	           $(this).find(".breadwrap").remove();
 	      }
 	    });
 
 	  }
-	} 
+	}
 	};
 */
 
